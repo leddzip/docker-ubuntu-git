224ebdf6879518b2ec249afa9d454b794313bff4
Leddzip <leddzip@gmail.com>
2021-12-15 20:26:56 +0100

    fixing little issue with test due to incorrect env

--- build-scripts/test-ubuntu-version.sh
+++ build-scripts/test-ubuntu-version.sh
@@ -1,4 +1,4 @@
-set -e
+set -ex
 
 #===============================
 # This scripts assert that the correct ubuntu version is used in
@@ -7,17 +7,18 @@ set -e
 
 IMAGE_NAME=$1
 
-if [[ -z "${UBUNTU_VERSION}" ]]; then
+if [[ -z "${UBUNTU_DOCKER_TAG}" ]]; then
   exit 1
 else
+  echo "${UBUNTU_DOCKER_TAG}"
   ubuntu_version=$(docker run --rm "$IMAGE_NAME" cat /etc/os-release | grep "VERSION_ID")
 
-  if [[ "ubuntu_version" == "VERSION_ID=\"${UBUNTU_DOCKER_TAG}\"" ]]
+  if [[ "$ubuntu_version" == "VERSION_ID=\"${UBUNTU_DOCKER_TAG}\"" ]]
   then
-    echo "poetry version is correct"
+    echo "ubuntu version is correct"
     echo "expected $UBUNTU_DOCKER_TAG, got $ubuntu_version"
   else
-    >&2 echo "poetry version is NOT correct"
+    >&2 echo "ubuntu version is NOT correct"
     >&2 echo "expected $UBUNTU_DOCKER_TAG, got $ubuntu_version"
     exit 2
   fi
